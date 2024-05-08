974b932af6cfb032d0dfd2db77b5e3e2f88460d5
echague22 <echague.k.bsinfotech@gmail.com>
2024-05-09 00:22:56 +0800

    Working Login and Logout with Display Data

--- IPAT2C/dcrm/urls.py
+++ IPAT2C/dcrm/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.first, name='home'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('logout/', views.logout_user, name='logout')
 ]
