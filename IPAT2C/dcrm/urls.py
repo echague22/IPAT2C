82850460eb53bb1c9dc1b1f0646deed2ea3831ca
echague22 <echague.k.bsinfotech@gmail.com>
2024-05-11 00:38:02 +0800

    WORKING CRUD

--- IPAT2C/dcrm/urls.py
+++ IPAT2C/dcrm/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.first, name='home'),
+    path('', views.home, name='home'),
     path('register/', views.register, name='register'),
-    path('logout/', views.logout_user, name='logout')
+    path('logout/', views.logout_user, name='logout'),
+    path('add_records/', views.add_record, name='add_records'),
+    path('update_records/<int:pk>', views.update_record, name='update_records'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
 ]
